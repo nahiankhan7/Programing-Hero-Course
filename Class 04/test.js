453bb68a796857034f548831aa1cabda916f9399
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-10 09:40:18 +0600

    course updated

--- Class 04/test.js
+++ Class 04/test.js
@@ -1,8 +1,22 @@
-const person = {
-  name: "Jack",
-  age: 24,
-  address: "Baler Hat",
-  phone: 889231467,
-  friendList: ["john", "lari", "neo"],
-};
-console.log(person.friendList);
+// const person = {
+//   name: "Jack",
+//   age: 24,
+//   address: "Baler Hat",
+//   phone: 889231467,
+//   friendList: ["john", "lari", "neo"],
+// };
+// const { address, phone } = person;
+// console.log(address, phone);
+
+const friends = [
+  "Oliver",
+  "Jake",
+  "Noah",
+  "James",
+  "William",
+  "Damian",
+  "Daniel",
+  "Thomas",
+];
+const [x, y, z, ...w] = friends;
+console.log(...w);
