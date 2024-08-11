b2238532037dd87869d78cb656892337b8fcc865
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-11 12:18:44 +0600

    scope

---
+++ Class 06/test.js
@@ -0,0 +1,11 @@
+// function sum(first, second) {
+//   let result = first + second;
+//   return result;
+// }
+// const output = sum(3, 12);
+// console.log(output);
+
+// const addName = ["Alex", "John", "Peri"];
+// const randomIndex = Math.floor(Math.random() * addName.length);
+// const winner = addName[randomIndex];
+// console.log(`The winner is: ${winner}`);
