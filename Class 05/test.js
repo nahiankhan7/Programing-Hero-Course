e4e8e1db461cacb05d8fc12c637c8d505acab95e
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-11 09:56:09 +0600

    map filter array

---
+++ Class 05/test.js
@@ -0,0 +1,16 @@
+// const number = [10, 20, 30, 40, 50];
+// const output = [];
+// for (let i = 0; i < number.length; i++) {
+//   const element = number[i];
+//   const result = element * element;
+//   output.push(result);
+// }
+// console.log(output);
+
+// const number = [10, 20, 30, 40, 50];
+// const result = number.map((x) => x * x);
+// console.log(result);
+
+const number = [10, 20, 30, 40, 50];
+const result = number.filter((x) => x > 10);
+console.log(result);
