5cab76edfa67e5f09e2ffe658ad7ec692bd254aa
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-11 13:39:35 +0600

    class 06

--- Class 06/test.js
+++ Class 06/test.js
@@ -9,3 +9,19 @@
 // const randomIndex = Math.floor(Math.random() * addName.length);
 // const winner = addName[randomIndex];
 // console.log(`The winner is: ${winner}`);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// const part = numbers.slice(2, 7);
+// const remove = numbers.splice(2, 5);
+// const join = numbers.join(" ");
+// console.log(join);
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let index = 0; index < number.length; index++) {
+  const element = number[index];
+  if (element > 5) {
+    break;
+    // continue;
+  }
+  console.log(element);
+}
