48a184871b6230380d457506d759b68faaecdfc2
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-11 10:14:46 +0600

    array of object

--- Class 05/test.js
+++ Class 05/test.js
@@ -11,6 +11,24 @@
 // const result = number.map((x) => x * x);
 // console.log(result);
 
-const number = [10, 20, 30, 40, 50];
-const result = number.filter((x) => x > 10);
-console.log(result);
+// const number = [10, 20, 30, 40, 50];
+// const result = number.filter((x) => x > 10);
+// console.log(result);
+
+const students = [
+  {
+    id: 99,
+    name: "Hero",
+  },
+  {
+    id: 104,
+    name: "Pappu",
+  },
+  {
+    id: 102,
+    name: "Noor",
+  },
+];
+const names = students.map((s) => s.name);
+const bigger = students.filter((x) => x.id >= 100);
+console.log(bigger);
