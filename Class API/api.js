fa44ed037f9ead8ead34c2dcb8254350def4a1c2
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-12 10:51:29 +0600

    post and get api

--- Class API/api.js
+++ Class API/api.js
@@ -3,18 +3,42 @@
 // const jSonUser = JSON.parse(userJson);
 // console.log(jSonUser);
 
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((response) => response.json())
-  .then((json) => displayUser(json));
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((json) => displayUser(json))
+//   .catch((error) => console.log(error));
 
-function displayUser(users) {
-  const userNames = users.map((user) => user.name);
+// function displayUser(users) {
+//   const userNames = users.map((user) => user.name);
+//   const ulElement = document.getElementById("allUsers");
 
-  userNames.forEach((user) => {
-    const ulElement = document.getElementById("allUsers");
-    const liElement = document.createElement("li");
+//   userNames.forEach((user) => {
+//     const liElement = document.createElement("li");
+//     liElement.textContent = user;
+//     ulElement?.appendChild(liElement);
+//   });
+// }
 
-    liElement.textContent = user;
-    ulElement?.appendChild(liElement);
-  });
+const submitButton = document.getElementById("subButton");
+submitButton?.addEventListener("click", function () {
+  const title = document.getElementById("titleId")?.value;
+  const bodyContent = document.getElementById("bodyId")?.value;
+
+  const postInfo = {
+    title: title,
+    body: bodyContent,
+  };
+  postToServer(postInfo);
+});
+
+function postToServer(postInfo) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(postInfo),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
