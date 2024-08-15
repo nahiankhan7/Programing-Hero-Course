2b7d6ec01fe9984493579e6739c02f893038b746
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-15 09:48:47 +0600

    async await api

--- Class 08/test.js
+++ Class 08/test.js
@@ -1,8 +1,33 @@
-const tstObject = {
-  name: "Akkass",
-  getName: function () {
-    return "Mr. " + this.name;
-  },
-};
-tstObject.getName();
-console.log();
+// function loadData() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       displayData(data);
+//     });
+// }
+// loadData();
+
+function displayData(data) {
+  const parentNode = document.getElementById("myList");
+  data.forEach((user) => {
+    const item = document.createElement("li");
+    item.innerText = user.name;
+    parentNode?.appendChild(item);
+  });
+}
+
+// async function hello(name) {
+//   return "hello " + name;
+// }
+// const getHi = hello("Mofiz");
+// console.log(getHi);
+// getHi.then((res) => console.log(res));
+
+async function usersData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data;
+}
+usersData().then((data) => {
+  displayData(data);
+});
